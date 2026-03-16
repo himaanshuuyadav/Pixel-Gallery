3c61de635bf49355fa9813f2c6f637db9ed56e35
Himanshu Yadav <himaanshuuyadav@gmail.com>
2026-03-16 22:49:27 +0530

    chore(app): adjust application initialization path
    Update application-level initialization logic to match current diagnostics and runtime setup.

--- app/src/main/java/com/prantiux/pixelgallery/PixelGalleryApplication.kt
+++ app/src/main/java/com/prantiux/pixelgallery/PixelGalleryApplication.kt
@@ -1,11 +1,9 @@
 package com.prantiux.pixelgallery
 
 import android.app.Application
-import com.prantiux.pixelgallery.startup.StartupTrace
 
 class PixelGalleryApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        StartupTrace.markStage("Application started", once = true)
     }
 }
